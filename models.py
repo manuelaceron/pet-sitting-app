88426387b4a4e54a93e821b741ed2e41809c5cc8
manuelaceron <mafe-cv@hotmail.com>
2024-10-24 18:00:29 +0200

    Add SQL connection, model and signUp method

---
+++ models.py
@@ -0,0 +1,11 @@
+from . import db
+
+
+#User class represent User table in model
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(100), unique =True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+
