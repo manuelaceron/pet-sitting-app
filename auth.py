aad5a9d3cee1dbfa5a365e73a5bd6d5f50a61517
manuelaceron <mafe-cv@hotmail.com>
2024-10-24 15:53:02 +0200

    Delete old images, add auth templates

--- auth.py
+++ auth.py
@@ -1,10 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, request, redirect
 
 authBp = Blueprint('auth', __name__)
 
 @authBp.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+@authBp.route('/signup', methods=['POST'])
+def signup_post():
+    # get the info captured from the html form to server..
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    print(email, name, password)
+
+    # TODO: send to database
+
+    return redirect(url_for('auth.login'))
+    
 
 @authBp.route('/login')
 def login():
