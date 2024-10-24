602ab7520659c3658fa249d10d102989b7655479
manuelaceron <mafe-cv@hotmail.com>
2024-10-24 18:02:17 +0200

    Add SQL connection, model and signUp method

--- __init__.py
+++ __init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
 
 # Create flask instance
 def create_app():
     
     # Instance of flask app
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'secret-key' #strong, random key in production to prevent attacks like session hijacking or cross-site request forgery, typically in config files
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    # Initialize the database with the app
+    db.init_app(app) 
+
+    with app.app_context():  
+        db.create_all()
+
     
     # Register Blueprints into our app instance
     from .main import mainBp as main_blueprint
--- auth.py
+++ auth.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, url_for, request, redirect
+from werkzeug.security import generate_password_hash
+from .models import User
+from . import db
 
 authBp = Blueprint('auth', __name__)
 
@@ -14,9 +17,19 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    print(email, name, password)
-
     # TODO: send to database
+
+    # Query user table to get the first row matching email
+    user = User.query.filter_by(email=email).first()
+    
+    if user:
+        print('User already Exists!')
+    
+    else:
+        print('Create new user')
+        new_user = User(email=email, name=name, password=generate_password_hash(password, method='pbkdf2:sha256'))
+        db.session.add(new_user)
+        db.session.commit()
 
     return redirect(url_for('auth.login'))
     
@@ -24,6 +37,16 @@ def signup_post():
 @authBp.route('/login')
 def login():
     return render_template('login.html')
+
+@authBp.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')    
+    password = request.form.get('password')
+
+    print(email, password)
+
+    return redirect(url_for('main.profile'))
+
 
 @authBp.route('/logout')
 def logout():
